02e166808713ab923097a68d6a42f1721568b04a
Pierre GILLON <contact@pierregillon.com>
2022-10-19 12:39:38 +0200

    feat: add controller version

---
+++ src/Bootstrap.WebApi/Controllers/V1/VersionController.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bootstrap.WebApi.Controllers.V1;
+
+[ApiController]
+[Route("v{v:apiVersion}/version")]
+[ApiVersion("1.0")]
+public class VersionController : ControllerBase
+{
+    [HttpGet]
+    public string? Get() => Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
+}
